9975ce6944d401f9db3aec173f818664c4b017b0
ronny-souza <ronnymarinho2@gmail.com>
2021-01-14 19:47:01 -0300

    Adicionando exercicio 02

---
+++ Exercicio02.py
@@ -0,0 +1,10 @@
+# Calcule e imprima a soma de todos os números de 5 a 15
+
+soma = 0
+contador = 5
+
+while contador <= 15:
+    soma = soma + contador
+    contador += 1
+
+print("Valor da soma:", soma)
