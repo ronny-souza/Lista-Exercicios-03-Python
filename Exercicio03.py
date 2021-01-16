80f327437dd8765b6cccb60775496f3dd5297e3b
ronny-souza <ronnymarinho2@gmail.com>
2021-01-16 11:46:10 -0300

    Adicionando exercicio 03

---
+++ Exercicio03.py
@@ -0,0 +1,10 @@
+# Utilize o loop while para calcular o valor de 8³, sabendo que equivale a 8*8*8
+
+numero = 8
+multiplica = 1
+i = 1
+
+while i <= 3:
+    multiplica *= numero
+    i += 1
+print(multiplica) 
